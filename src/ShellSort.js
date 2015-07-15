2be93832e007fe116741ee4e48c572889db5e178
Hiroki Kumamoto <kumabook@live.jp>
2015-07-15 21:56:39 +0900

    SortVisualiser shows  num of compare and exchange

--- src/ShellSort.js
+++ src/ShellSort.js
@@ -1,12 +1,15 @@
 function* sort(a, less) {
+  var ec = 0; //numOfExch
+  var cc = 0; //numOfComp
   var N = a.length;
   var h = 1;
   while (h < N / 3) h = 3 * h + 1; // 1, 4, 13, 40, 121, 364, ...
   while (h >= 1) {
     for (var i = h; i < N; i++) {
       for (var j = i; j >= h; j -= h) {
-        yield { selections: [j, j - h], h: h };
+        yield { selections: [j, j - h], h: h, numOfExch: ec, numOfComp: ++cc };
         if (less(a[j], a[j - h])) {
+          ++ec;
           exch(a, j, j - h, h);
         } else {
           break;
