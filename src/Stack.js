91f163d174b2b7e3301e52c3fa9fbef76aace58e
Hiroki Kumamoto <kumabook@live.jp>
2016-01-09 16:27:55 +0900

    Add doc coment to Stack

--- src/Stack.js
+++ src/Stack.js
@@ -1,3 +1,10 @@
+/**
+ * Create a stack
+ * @constructor
+ * @param params parameter
+ * @param params.capacity initial interal array size.
+ * @classdesc Represent a LIFO stack with resizing array.
+ */
 var Stack = function(_params) {
   var params    = _params || {};
   this.capacity = params.capacity || 2;
@@ -5,14 +12,26 @@ var Stack = function(_params) {
   this.size     = 0;
 };
 
+/**
+ * Returns the size of items.
+ * @returns {Number}
+ */
 Stack.prototype.getSize = function() {
   return this.size;
 };
 
+/**
+ * Returns the capacity of internal array.
+ * @returns {Number}
+ */
 Stack.prototype.getCapacity = function() {
   return this.capacity;
 };
 
+/**
+ * Is this stack empty?
+ * @returns {Boolean}
+ */
 Stack.prototype.isEmpty = function() {
   return this.size == 0;
 };
@@ -27,12 +46,20 @@ Stack.prototype.resize = function(capacity) {
   this.buffer = temp;
 };
 
+/**
+ * push an item.
+ * @param {*} val item.
+ */
 Stack.prototype.push = function(val) {
   var l = this.capacity;
   if (this.size == l) this.resize(2 * l);
   this.buffer[this.size++] = val;
 };
 
+/**
+ * pop an item.
+ * @returns {*} val item.
+ */
 Stack.prototype.pop = function() {
   var l = this.capacity;
   var v = this.buffer[this.size - 1];
@@ -40,7 +67,9 @@ Stack.prototype.pop = function() {
   if (this.size > 0 && this.size == l / 4) this.resize(l / 2);
   return v;
 };
-
+/**
+ * Returns (but does not remove) the last-in item.
+ */
 Stack.prototype.peek = function() {
   return this.buffer[this.size - 1];
 };
