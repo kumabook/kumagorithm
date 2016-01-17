dac07f26bf5883e89c1e516e6a681af047e00d7a
Hiroki Kumamoto <kumabook@live.jp>
2016-01-17 23:09:41 +0900

    MergeSortVisualiser use react-motion

--- src/components/MergeSortVisualiser.jsx
+++ src/components/MergeSortVisualiser.jsx
@@ -1,8 +1,13 @@
-var React          = require('react');
 var MergeSort      = require('../MergeSort');
 var SortVisualiser = require('./SortVisualiser');
+var React          = require('react');
+var ReactDOM       = require('react-dom');
+var ReactMotion    = require('react-motion');
+var Motion         = ReactMotion.Motion;
+var spring         = ReactMotion.spring;
+var presets        = ReactMotion.presets;
 
-var width  = 10;
+var width  = 20;
 var height = 5;
 var radius = 15;
 var N      = 30;
@@ -35,12 +40,62 @@ var MergeSortVisualiser = React.createClass({
 
     if (s.length == 0 || lo === undefined || hi === undefined) return;
     var data = [
-      {d: 'M ' + (lo*10) + ' 0 v 250', stroke: 'black', strokeWidth: 1},
-      {d: 'M ' + ((hi+1)*10) + ' 0 v 250', stroke: 'black', strokeWidth: 1}
+      {key: 1, d: 'M ' + (lo * width) + ' 0 v 250', stroke: 'black', strokeWidth: 1},
+      {key: 2, d: 'M ' + ((hi+1) * width) + ' 0 v 250', stroke: 'black', strokeWidth: 1}
     ];
     return data.map((d) => {
-      return <path d={d.d} stroke={d.stroke} strokeWidth={d.strokeWidth}></path>
+      return <path key={d.key}
+                   d={d.d} stroke={d.stroke} strokeWidth={d.strokeWidth}></path>
     });
+  },
+  renderMain: function() {
+    var width = this.props.width, height = this.props.height;
+    var MAX = this.props.MAX;
+    var colors = this.props.colors;
+    var data = [];
+    var selections = this.state.value.selections || [];
+    var extraRects = [];
+    for (var i = 0, l = this.state.array.length; i < l; i++) {
+      var h = this.state.array[i] * height;
+      var color = colors.normal;
+      if (selections[0] == i) color = colors.highlight1;
+      if (selections[1] == i) color = colors.highlight2;
+      if (selections[2] == i) color = colors.highlight3;
+      if (selections[3] == i) color = colors.highlight4;
+      data.push({
+        key: i,
+        x: i * width,
+        y: height * MAX - h,
+        h: h,
+        color: color
+      });
+    }
+    return (
+      <g>
+        {data.map((d) => {
+           let style = {
+             x: spring(d.x),
+             y: spring(d.y),
+             width: width,
+             h: d.h,
+             color: d.color
+           };
+           return (
+             <Motion key={d.key}
+                     style={style}>
+               {d => {
+                  return (<rect
+                              x={d.x} y={d.y} width={width} height={d.h}
+                              stroke="#136FFF" strokeWidth="1" fill={d.color}>
+                  </rect>);
+                }
+               }
+             </Motion>
+           );
+         })
+        }
+      </g>
+    );
   },
   renderAux: function() {
     if (!this.state.value.aux) return;
@@ -56,18 +111,42 @@ var MergeSortVisualiser = React.createClass({
       if (selections[0] == i) color = colors.highlight2;
       if (selections[1] == i) color = colors.highlight3;
       data.push({
-        key: i,
+        key: array[i],
         x: i * width,
         y: height * MAX - h,
         h: h,
         color: color
       });
     }
-    return data.map((d) => {
-      return <rect key={d.key}
-                 x={d.x} y={d.y} width={width} height={d.h}
-                 strokeWidth="1" stroke="#136FFF" fill={d.color}></rect>
-    });
+    return (
+      <g>
+        {data.map((d) => {
+           let style = {
+             x: spring(d.x),
+             y: spring(d.y),
+             width: width,
+             h: d.h,
+             color: d.color
+           };
+           return (
+             <Motion key={d.key}
+                     style={style}>
+               {d => {
+                  return (
+                    <rect key={d.key}
+                          x={d.x} y={d.y} width={width} height={d.h}
+                          strokeWidth="1"
+                          stroke="#136FFF"
+                          fill={d.color}></rect>
+                  );
+                }
+               }
+             </Motion>
+           );
+         })
+        }
+      </g>
+    );
   },
   getExtra: function() {
     var s = this.state.value.selections || [];
@@ -95,7 +174,7 @@ var MergeSortVisualiser = React.createClass({
         <svg ref="svg"
              width={this.state.array.length * width}
              height={height * MAX}>
-          {this.renderItems()}
+          {this.renderMain()}
           {this.renderLine()}
         </svg>
         <br/>
