1038da906a7f9be7bc41a8e2155e509669a2e45e
Hiroki Kumamoto <kumabook@live.jp>
2016-01-17 22:29:36 +0900

    Use react-motion for SortVisualiser

--- src/components/SortVisualiser.jsx
+++ src/components/SortVisualiser.jsx
@@ -1,6 +1,9 @@
-var React    = require('react');
-var ReactDOM = require('react-dom');
-
+var React       = require('react');
+var ReactDOM    = require('react-dom');
+var ReactMotion = require('react-motion');
+var Motion      = ReactMotion.Motion;
+var spring      = ReactMotion.spring;
+var presets     = ReactMotion.presets;
 var width  = 10;
 var height = 5;
 var radius = 15;
@@ -39,8 +42,9 @@ var SortVisualiser = {
   initArray: function() {
     var N = this.props.N, MAX = this.props.MAX;
     this.state.array = [];
-    for (var i = 0; i < N; i++) {
-      this.state.array.push(~~(Math.random() * MAX));
+    while (this.state.array.length < N) {
+      let val = ~~(Math.random() * MAX);
+      if (!this.state.array.includes(val)) this.state.array.push(val);
     }
     this.generator = this.props.sort.sort(this.state.array, (v, w) => {
       return v < w;
@@ -52,6 +56,7 @@ var SortVisualiser = {
     var colors = this.props.colors;
     var data = [];
     var selections = this.state.value.selections || [];
+    var extraRects = [];
     for (var i = 0, l = this.state.array.length; i < l; i++) {
       var h = this.state.array[i] * height;
       var color = colors.normal;
@@ -60,36 +65,57 @@ var SortVisualiser = {
       if (selections[2] == i) color = colors.highlight3;
       if (selections[3] == i) color = colors.highlight4;
       data.push({
-        key: i,
+        key: this.state.array[i],
         x: i * width,
         y: height * MAX - h,
         h: h,
         color: color
       });
     }
-    var rects = data.map((d) => {
-      return <rect key={d.key}
-                   x={d.x} y={d.y} width={width} height={d.h}
-                   stroke="#136FFF" strokeWidth="1" fill={d.color}>
-      </rect>
-    });
     if (selections.length >= 2) {
       var lo = Math.min(selections[0], selections[1]);
       var hi = Math.max(selections[0], selections[1]);
-      rects.push(<rect key="selection-1"
-                       x={lo * width} y="0"
-                       width={(hi - lo + 1) * width} height={height * MAX}
-                       fill="gray" fillOpacity="0.25">
-      </rect>);
+      extraRects.push(
+        <rect key="selection-1"
+              x={lo * width} y="0"
+              width={(hi - lo + 1) * width} height={height * MAX}
+              fill="gray" fillOpacity="0.25">
+        </rect>);
     }
     if (selections.length >= 4) {
-      rects.push(<rect key="selection-2"
+      extraRects.push(<rect key="selection-2"
                        x={selections[3] * width} y="0"
                        width={width} height={height * MAX}
                        fill="gray" fillOpacity={this.state.value.partitionEnd ? 0.5 : 0}>
       </rect>);
     }
-    return rects;
+    return (
+      <g>
+        {data.map((d) => {
+           let style = {
+             x: spring(d.x),
+             y: spring(d.y),
+             width: width,
+             h: d.h,
+             color: d.color
+           };
+           return (
+             <Motion key={d.key}
+                     style={style}>
+               {d => {
+                  return (<rect
+                                x={d.x} y={d.y} width={width} height={d.h}
+                                stroke="#136FFF" strokeWidth="1" fill={d.color}>
+                  </rect>);
+                }
+               }
+             </Motion>
+           );
+         })
+        }
+        {extraRects}
+      </g>
+    );
   },
   next: function() {
     var value = this.generator.next().value;
